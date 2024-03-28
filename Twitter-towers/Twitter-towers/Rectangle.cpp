ca2c1761bc4c9ff8da1a9f411928a4595dfd9a17
TziporaPasternak <144274640+thipora@users.noreply.github.com>
2024-03-28 18:31:13 +0200

    question-1

---
+++ Twitter-towers/Twitter-towers/Rectangle.cpp
@@ -0,0 +1,17 @@
+﻿#include "Rectangle.h"
+
+Rectangle::Rectangle(int h, int w)
+{
+	height = h;
+	width = w;
+}
+
+int Rectangle::perimeter()
+{
+	return (height + width) * 2;
+}
+
+int Rectangle::area()
+{
+	return height * width;
+}
